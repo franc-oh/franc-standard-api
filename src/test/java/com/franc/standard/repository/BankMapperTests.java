ae13174aca3f25c447d0ce544782dafd1556ebfb
france1988 <france1988@naver.com>
2023-05-25 11:39:41 +0900

    개발완료

--- src/test/java/com/franc/standard/repository/BankMapperTests.java
+++ src/test/java/com/franc/standard/repository/BankMapperTests.java
@@ -4,9 +4,9 @@ package com.franc.standard.repository;
 import com.franc.standard.vo.BankVO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 @ExtendWith(SpringExtension.class)
-@MybatisTest(properties = {"spring.profiles.active=test", "jasypt.encryptor.password=test"})
+@SpringBootTest(properties = {"spring.profiles.active=test", "jasypt.encryptor.password=test"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 public class BankMapperTests {
